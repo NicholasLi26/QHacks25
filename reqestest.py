507e0059887e2552539f7965b14e935e0ee7ff71
NicholasLi <nicholas@lifamily.ca>
2025-01-26 00:45:36 -0500

    ok gemini should actually be done

--- reqestest.py
+++ reqestest.py
@@ -2,11 +2,11 @@ import PIL.Image
 import google.generativeai as genai
 import time
 
-# with open('keys/gemini.txt', 'r') as file:
-#     key = file.read()
+with open('keys/gemini.txt', 'r') as file:
+    key = file.read()
 
-# genai.configure(api_key=key)
-# model = genai.GenerativeModel(model_name = "gemini-1.5-flash")
+genai.configure(api_key=key)
+model = genai.GenerativeModel(model_name = "gemini-1.5-flash")
 
 def ask_gemini(model, prompt, images):
     responses = []
@@ -45,13 +45,14 @@ def process_gemini(responses):
     months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
 
     for response in responses:
-
         isitschedule = 0
 
         for i, day in enumerate(days):
             if day in response[0].lower():
                 schedule[i] = response[1:]
                 isitschedule = 1
+
+        if isitschedule==1: return schedule
 
         if isitschedule==0:
             course_code = response[-1]
@@ -78,19 +79,23 @@ def process_gemini(responses):
                     events.append([response[i], response[i+int(len(response)/2)]])
                     events[-1].append(course_code)
 
-    return (schedule, events)
+    return events
 
 # ask_gemini test
 images = [
-    "images/syllabus1.png",
-    "images/syllabus2.png",
-    "images/syllabus3.png",
+    # "images/syllabus1.png",
+    # "images/syllabus2.png",
+    # "images/syllabus3.png",
+    "images/mon.png",
+    "images/tue.png",
+    "images/wed.png",
+    "images/thu.png",
+    "images/fri.png",
 ]
 
 prompt_schedule = """
 What day of the week is this, what classes do I have, and what are the start and end times of each class? Please print it in a list format like this: day of the week, class 1, class 1 start time, class 1 end time, class 2, class 2 start time, etc. for all class times, with a line break after each item. Please only print the list, no other text. Thank you!
 """
-
 
 prompt_syllabus = """
 What are the important dates for this course? Please provide me with a list describing what is happening on what day. Please print each item in a list format like this: date 1 (line break), date 2 if there is a date range (line break), description (line break) (line break). A line should not contain more than one date, so all second dates must be written on a new line. All dates should be formatted as: Month Day. Please only print the list, no other text. If there are no important dates, please respond with NO_IMPORTANT_DATES. Thank you!
@@ -108,3 +113,13 @@ What are the important dates for this course? Please provide me with a list desc
 # print(schedule)
 # print("\n\nHAIIIII")
 # print(events)
+
+responses = ask_gemini(model, prompt_schedule, images)
+
+for response in responses:
+    print(response)
+    print("\n\n")
+
+schedule_or_events = process_gemini(responses)
+
+print(schedule_or_events)
